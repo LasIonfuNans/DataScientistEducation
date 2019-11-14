14ccd6e42d9ca60c744fc12305cacc16052d8919
LasIonfuNans <johndoetheorem@gmail.com>
2019-11-14 22:39:38 +0900

    コードフォーマットしてアップし直し

--- 01/01_02_05.py
+++ 01/01_02_05.py
@@ -1,24 +1,26 @@
-#%%
-#01_02_05_01
+# 01_02_06
+
+from functools import lru_cache
 
 counter = 0
 
-#再帰関数の例
+
+# 再帰関数の例
 def calc_fib(n):
     global counter
     counter += 1
-    if n in [1,2]:
+    if n in [1, 2]:
         return 1
     else:
-        return calc_fib(n-1) + calc_fib(n-2)
+        return calc_fib(n - 1) + calc_fib(n - 2)
 
-print('フィボナッチ数：',calc_fib(25))
+
+print('フィボナッチ数：', calc_fib(25))
 print(counter, "回の関数呼び出し")
 # フィボナッチ数：75025
 # 150049 回の関数呼び出し
 
-from functools import lru_cache
-counter = 0
+
 @lru_cache(maxsize=1024)
 def fib2(n):
     global counter
@@ -27,39 +29,36 @@ def fib2(n):
         return 1
     return fib2(n - 1) + fib2(n - 2)
 
-print('フィボナッチ数：',fib2(25))
+
+print('フィボナッチ数：', fib2(25))
 print(counter, "回の関数呼び出し")
 # フィボナッチ数：75025
 # 25 回の関数呼び出し
 
-#%%
-#01_02_05_02
+print((lambda a, b: a * b)(3, 10))
 
-print((lambda a,b:a*b)(3,10))
 
 def calc_double(x):
-    return x**2
+    return x ** 2
 
-for num in [1,2,3,4]:
+
+for num in [1, 2, 3, 4]:
     print(calc_double(num))
 
-print(list(map(calc_double,[1,2,3,4])))
+print(list(map(calc_double, [1, 2, 3, 4])))
+print(list(map(lambda x: x ** 2, [1, 2, 3, 4])))
 
-print(list(map(lambda x:x**2,[1,2,3,4])))
-
-#%%
-#01_02_05_Practice_01_01
 
 input_str = "data Science"
 
 for str_index in range(len(input_str)):
     print(input_str[str_index])
 
-#%%
-#01_02_05_Practice_01_02
 maxN = 55
-def sumfromone(N):
-    return int((N*(N+1))/2)
-print(sumfromone(maxN))
 
 
+def sum_from_one(N):
+    return int((N * (N + 1)) / 2)
+
+
+print(sum_from_one(maxN))
--- 01/ComprehensiveProblem01.py
+++ 01/ComprehensiveProblem01.py
@@ -1,15 +1,17 @@
-#%%
 import math
+
 
 def prime_check(i):
     if i == 1:
         print(i)
     else:
-        for j in range(2,int(math.sqrt(i))+1):
+        for j in range(2, int(math.sqrt(i)) + 1):
             if i % j == 0:
                 print(i)
                 return
-        print(i,":Prime")
+        print(i, ":Prime")
 
-for k in range(1,50):
-    prime_check(k)
+
+if __name__ == '__main__':
+    for k in range(1, 50):
+        prime_check(k)
